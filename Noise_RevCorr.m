48254f2aa099655bb7b3db7644ccce8e9d9b109b
Byron Price <byron.h.price@gmail.com>
2016-12-19 15:21:20 -0500

    change waiting times distribution

--- Noise_RevCorr.m
+++ Noise_RevCorr.m
@@ -125,8 +125,8 @@ Grey = 127*ones(minPix,minPix);
 Priority(9);
 % Retrieve monitor refresh duration
 ifi = Screen('GetFlipInterval', win);
-flipIntervals = flipInterval-0.05+exprnd(0.05,[numStimuli,1]);
-WaitTimes = WaitTime-0.1+exprnd(0.1,[numStimuli,1]);
+flipIntervals = flipInterval-0.1+exprnd(0.1,[numStimuli,1]);
+WaitTimes = WaitTime+exprnd(0.25,[numStimuli,1]);
 
 usb.startRecording;
 WaitSecs(5);
