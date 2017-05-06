d4db334fec026e03e8ed0471ef9463306e2b83d1
Byron Price <byron.h.price@gmail.com>
2017-05-06 17:17:53 -0400

    Save your data when you do simulations

--- RevCorrCossell.m
+++ RevCorrCossell.m
@@ -1,6 +1,9 @@
 % RevCorr_Cossell.m
 
-fileName = {'13_05_22','13_05_20','13_05_19','13_05_14'};
+fileName = {'13_05_22','13_05_20','13_05_19','13_05_14',...
+    '13_05_13','13_05_10','13_05_07','13_05_06','13_05_01','13_04_30',...
+    '13_04_29','13_04_26','13_04_21','13_04_17','13_04_16','13_04_14',...
+    '13_04_05'};
 nExps = length(fileName);
 [csCaData, sParameters, tfImageStack] = ...
    Load_Cossell_etal_2015_NaturalImageResponses(fileName);
@@ -36,41 +39,39 @@ for ii=1:nExps
                 tempMat = zeros(DIM(1),DIM(2));
                 tempMat(ll,kk) = 4;
                 if kk > 1 && kk < DIM(2)
-                    tempMat(kk-1,ll) = -1;
-                    tempMat(kk+1,ll) = -1;
+                    tempMat(ll,kk-1) = -1;
+                    tempMat(ll,kk+1) = -1;
                 end
                 if ll > 1 && ll < DIM(1)
-                    tempMat(kk-1,ll) = -1;
-                    tempMat(kk+1,ll) = -1;
+                    tempMat(ll-1,kk) = -1;
+                    tempMat(ll+1,kk) = -1;
                 end
                 L(bigCount,:) = tempMat(:)';bigCount = bigCount+1;
             end
         end
         
-        F = zeros(numNeurons,DIM(1)*DIM(2));
-        bigLambda = logspace(0,4,10);
-        train = round(numIms*0.7);
+        
+        bigLambda = logspace(3,7,15);
+        RMS = zeros(numNeurons,length(bigLambda));
+        F = zeros(numNeurons,length(bigLambda),DIM(1)*DIM(2));
+        
+        train = round(numIms*0.75);
         for kk=1:numNeurons
             r = reducedSpikeData(kk,61:60+train)';
             constraints = [r;zeros(DIM(1)*DIM(2),1)];
             tempA = convertedStack(61:60+train,:);
             
-            tempF = zeros(length(bigLambda),DIM(1)*DIM(2));
-            RMS = zeros(length(bigLambda),1);
             for ll=1:length(bigLambda)
                 A = [tempA;bigLambda(ll).*L];
-                tempF(ll,:) = A\constraints;clear A;
+                F(kk,ll,:) = A\constraints;clear A;
                 rTest = reducedSpikeData(kk,61+train:numIms)';
                 A = convertedStack(61+train:numIms,:);
-                RMS(ll) = norm(rTest-A*tempF(ll,:)')./sqrt(DIM(1)*DIM(2));
+                RMS(kk,ll) = norm(rTest-A*squeeze(F(kk,ll,:)))./sqrt(DIM(1)*DIM(2));
+                clear A;
             end
              clear tempA A;
-             [~,bestMap] = min(RMS);
-             F(kk,:) = tempF(bestMap,:);
-             figure();imagesc(reshape(F(kk,:),[DIM(1),DIM(2)]));
-             title(sprintf('Im Plane %d - Neuron %d',jj,kk));
         end
         newFileName = strcat(fileName{ii},'-',num2str(jj));
-        save(newFileName,'F','bigLambda');
+        save(newFileName,'F','bigLambda','DIM','RMS','reducedSpikeData');
     end
 end
