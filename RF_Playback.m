5e5b7227e85fe23ef99187bce54f7a816551ede4
byronprice <byron.h.price@gmail.com>
2017-07-26 14:46:58 -0400

    2-second pause after motion

--- RF_Playback.m
+++ RF_Playback.m
@@ -138,7 +138,7 @@ while tt <= numStimuli
     if tcpipClient.BytesAvailable > 0
         data = fread(tcpipClient,tcpipClient.BytesAvailable/8,'double');
         if sum(data) > 0
-            WaitSecs(4);
+            WaitSecs(2);
         else
             % Convert it to a texture 'tex':
             Img = reshape(S(tt,:),[DIM(1),DIM(2)]);
